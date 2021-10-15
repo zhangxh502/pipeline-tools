4edc2233b907ff5e0640aa95c72971e6ed82d00d
zhangxh502 <zmj1981123@163.com>
2021-10-15 21:47:47 +0800

    Update kube-apply.sh

--- kube-apply.sh
+++ kube-apply.sh
@@ -1,19 +1,5 @@
 cd /home/jenkins/workspace/$CI_PIPELINE_NAME
 
-if [ -z "$CI_DEPLOYMENT_CONFIG" ] && [ -z "$CI_DEPLOYMENT_CONTENT" ]
-then
-        echo "Config or content must be specified!"
-        exit 1
-fi
-
-if [ -n "$CI_DEPLOYMENT_CONTENT" ]
-then
-CI_DEPLOYMENT_CONFIG="./.yicloud-deployment.yaml"
-cat <<EOF> $CI_DEPLOYMENT_CONFIG
-$YAML_CONTENT
-EOF
-fi
-
 if [ ! -f "$CI_DEPLOYMENT_CONFIG" ]
 then
         echo "File \"$CI_DEPLOYMENT_CONFIG\" not found! "
@@ -22,4 +8,5 @@ fi
 
 sed -i 's^${CI_BUILD_NUMBER}^'"$CI_BUILD_NUMBER^g" "$CI_DEPLOYMENT_CONFIG"
 
+echo $CI_DEPLOYMENT_CONFIG
 kubectl apply -f "$CI_DEPLOYMENT_CONFIG"
